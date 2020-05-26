fd0e73b9b2872fa8e7dde30fcbe1bf1c59be14ca
Olivier Girardot <olive_girardot@hotmail.com>
2020-05-26 09:23:03 +0200

    added the booking model and the foreign key references

---
+++ db/migrate/20200526072041_create_bookings.rb
@@ -0,0 +1,13 @@
+class CreateBookings < ActiveRecord::Migration[6.0]
+  def change
+    create_table :bookings do |t|
+      t.string :start_
+      t.date, :date
+      t.date, :end_date
+      t.references, :flat
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
