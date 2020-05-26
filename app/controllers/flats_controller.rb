04dbd25febaf1bd616849e9b6f668c1539b0b546
Olivier Girardot <olive_girardot@hotmail.com>
2020-05-26 15:12:40 +0200

    added the map display on the index page

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,6 +1,13 @@
 class FlatsController < ApplicationController
   def index
     @flats = Flat.all
+
+    @markers = @flats.map do |flat|
+      {
+        lat: flat.latitude,
+        lng: flat.longitude
+      }
+    end
   end
 
   def show
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,3 +3,12 @@ require("@rails/activestorage").start()
 require("channels")
 
 import "bootstrap";
+
+import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
+import { initMapbox } from '../plugins/init_mapbox';
+
+document.addEventListener('turbolinks:load', () => {
+  initMapbox();
+})
+
+initMapbox();
